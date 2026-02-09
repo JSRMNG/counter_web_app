67321cdf5d9b8e88170e90cae499f9c33ccc3541
JSRMNG <gangulimayaank37@gmail.com>
2026-02-09 19:49:44 +0530

    Added JS function to increment only

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var incrementButton = document.querySelector(".counter-increment");
-
 var display = document.querySelector(".counter-display");
 
 var count = 0;
 
-incrementButton.addEventListener("click",
-  function () {
+incrementButton.addEventListener("click", function () {
   count = count + 1;
-  display.innnerText = count;
-  
-  }
-);
+  display.innerText = count;
+});
