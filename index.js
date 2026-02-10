a41d7fbe4f2bc54eba3876a840471e9af373a4ae
JSRMNG <gangulimayaank37@gmail.com>
2026-02-10 22:19:40 +0530

    Added decement function only

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var incrementButton = document.querySelector(".counter-increment");
 var display = document.querySelector(".counter-display");
+var decrementButton = document.querySelector(".counter-decrement");
+var display = document.querySelector(".counter-display");
 
 var count = 0;
 
 incrementButton.addEventListener("click", function () {
   count = count + 1;
+  display.innerText = count;
+});
+
+decrementButton.addEventListener("click", function () {
+
+  if (count > 0) {
+    count--;
+    display.innerText = count;
+  }
+
   display.innerText = count;
 });
